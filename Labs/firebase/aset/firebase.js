7ff2bba4b8b7174f0e6e9b52610ee2a328751b29
Rismawan Junandia <junandia@users.noreply.github.com>
2018-12-14 18:31:32 +0700

    Create firebase.js

---
+++ Labs/firebase/aset/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  	var config = {
+    	apiKey: "AIzaSyAwsBdI6C_84FqC4g0hlI-Dbjs1-KHP2HQ",
+    	authDomain: "junandiagroup-c5d11.firebaseapp.com",
+    	databaseURL: "https://junandiagroup-c5d11.firebaseio.com",
+    	projectId: "junandiagroup-c5d11",
+    	storageBucket: "junandiagroup-c5d11.appspot.com",
+    	messagingSenderId: "704885993894"
+  	};
+  	firebase.initializeApp(config);
